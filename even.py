b2cf8f8e71483ffba4f19fe9de776b1378559a34
vaseem14 <36194950+vaseem14@users.noreply.github.com>
2019-02-07 18:17:58 +0530

    Update even.py

--- even.py
+++ even.py
@@ -7,8 +7,15 @@ while True:
 	except:
 		print("Invalid input")
 		break
+c=0
 for x in range(a+1,b):
 	if x%2==0:
-		print(x,end=' ')
+		c=c+1
 		
+for x in range(a+1,b):
+	if x%2==0:
+		if c>0:
+			print(x,end=' ')
+		else:
+			print(x)
 		
