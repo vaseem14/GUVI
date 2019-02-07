bf04b8bbe314a58c4b921503e354d9cac511e2cd
vaseem14 <36194950+vaseem14@users.noreply.github.com>
2019-02-06 21:58:15 -0800

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -2,7 +2,7 @@ x=raw_input()
 a=x.isalpha()
 if len(x)==1:
 	if a==True:
-			if x in ('a', 'e', 'i', 'o', 'u'):
+			if x in ('a', 'e', 'i', 'o', 'u','A','E','I','O','U'):
 				print('Vowel')
 			else:
 				print('Consonant')
