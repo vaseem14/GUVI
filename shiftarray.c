9c2b7f0cb1d4377eb0169b26749189bc19a19c68
vaseem14 <36194950+vaseem14@users.noreply.github.com>
2019-02-08 12:02:18 +0530

    Create shiftarray.c

---
+++ shiftarray.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	int b,c,temp,a[100],i;
+	scanf("%d%d",&b,&c);
+	for(i=0;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+	temp=a[0];
+    for(i=0;i<b-1;i++)
+    {
+        a[i]=a[i+1];
+    }
+    a[b-1]=temp;
+ for(i=0;i<b;i++)
+    {
+        printf("%d ",a[i]);
+    }
+	return 0;
+}
