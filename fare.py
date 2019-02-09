e8cd02b5c658a8d13c60dcc828524ac16be05a1a
vaseem14 <36194950+vaseem14@users.noreply.github.com>
2019-02-09 22:31:40 +0530

    Update fare.py

--- fare.py
+++ fare.py
@@ -10,13 +10,20 @@ while True:
 		break
 x=x*60
 b=x+y
-if (a<5):
-	print("Bill amount:")
-	print(75+b)
+if (b<=0):
+	print("Invalid input")
 else:
-	a=a-5
-	c=a*8
-	print("Bill amount:")
-	print(75+c+b)
-	
+	if (a<5):
+		print("Bill amount:")
+		print(75+b)
+	elif (a==0):
+		print("Bill amount :0")
+	elif (a<0):
+		print("Invalid input")
+	else:
+		a=a-5
+		c=a*8
+		print("Bill amount:")
+		print(75+c+b)
+		
 
