1fe27c193097c2553d63a07571e0b9cc91c6730d
vaseem14 <36194950+vaseem14@users.noreply.github.com>
2019-02-10 15:07:46 +0530

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -1,9 +1,12 @@
 import itertools
+from textwrap import wrap
 a=input()
 a=list(a)
 b=list(itertools.permutations(a))
-b=list(set(b))
+d=""
 for i in range(len(b)):
-	for j in range(len(a)):
-		print(b[i][j],end="")
-	print("")
+	d=d.join(b[i])
+c=len(a)
+d=wrap(d,c)
+b=list(set(b))
+print('\n'.join(str(x) for x in d))
