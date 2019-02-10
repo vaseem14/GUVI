ee9bab59bddb20d9ecfc5559a50e8839c2d4b147
vaseem14 <36194950+vaseem14@users.noreply.github.com>
2019-02-10 14:22:22 +0530

    Update revStrings.py

--- revStrings.py
+++ revStrings.py
@@ -1,11 +1,5 @@
 a=input().split()
 b=[]
 for i in range(len(a)):
-	if(a[i].isalpha()):
-		c=a[i][::-1]
-		b.append(c)
-	else:
-		print("Invalid input")
-		
-b=list(set(b))
-print(' '.join(str(x) for x in b))
+	a[i]=a[i][::-1]
+print(' '.join(str(x) for x in a))
