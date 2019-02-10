1c7cbd96b6303da392a2691e3c176b219b5e18d7
vaseem14 <36194950+vaseem14@users.noreply.github.com>
2019-02-10 12:31:28 +0530

    Update indexcom.py

--- indexcom.py
+++ indexcom.py
@@ -12,7 +12,10 @@ for i in range(len(a)):
 	if (int(a[i])==i):
 		b.append(int(a[i]))
 if (len(a)==x):
-	b=list(set(b))
-	print(' '.join(str(x) for x in b))
+	if (len(b)==0):
+		print("-1")
+	else:
+		b=list(set(b))
+		print(' '.join(str(x) for x in b))
 else:
 	print("Invalid input")
